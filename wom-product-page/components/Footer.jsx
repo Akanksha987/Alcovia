c28e0310188fc6f1bf4b3395ddce73d6128529ca
Akanksha987 <agrawalakanksha23123@gmail.com>
2025-11-27 21:24:13 +0530

    chore: remove legacy wom product page

--- wom-product-page/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-white text-center py-6 mt-10">
-      <p>© 2025 Word of Mouth | All Rights Reserved</p>
-    </footer>
-  );
-}
